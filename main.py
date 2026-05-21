125db453c20290cf765f601be42a07dc87ea6b72
mharoon1434 <m.haroon01010@gmail.com>
2026-05-21 22:35:46 +0500

    basic info

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+#imports
+import pandas as pd
+import numpy as np
+import seaborn as sns
 
+# Basic info for each student
+class Student:
+    def __init__(self, id, name, mid, terminal, assigniment):
+        # data definition here
+    def cal_final(self):
+        # calculate final result
+    def compute_grade(self):
+        # calculate the grade
+    def convert_dict(self):
+        # convert data into dict
